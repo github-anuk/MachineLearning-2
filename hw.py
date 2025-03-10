504276ebf18b7a8d7d1beacf1ad972001adc1b36
github-anuk <160622798+github-anuk@users.noreply.github.com>
2025-03-10 14:35:24 +0100

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mp
+import seaborn as sns
+
+'''from sklearn.datasets import load_boston
+data=load_boston()
+print(data.keys())
+
+boston = pd.DataFrame(data.data, columns = data.feature_names)
+print(boston.head())
+boston["MEDV"] = data.target'''
+
+iris=pd.read_csv("iris.csv")
+
+iris['species']=iris['species'].map({'setosa':0,"versicolor":1,'virginica':2,})
+print(iris.head())
+
+#X = boston[['LSTAT','RM']]
+#y=boston["MEDV"]
+X=iris[["sepal_length","sepal_width","petal_length","petal_width"]]
+y=iris['species']
+print(X.head()) 
+print(y.head())
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=5)
+
+from sklearn.linear_model import LinearRegression
+linear_mdl=LinearRegression()
+linear_mdl.fit(X_train,y_train)
+
+from sklearn.metrics import mean_squared_error
+
+y_test_predict = linear_mdl.predict(X_test)
+
+rmse_linear_mdl=(np.sqrt(mean_squared_error(y_test,y_test_predict)))
+print("THE rmse is case of multivariable regression is",rmse_linear_mdl)
